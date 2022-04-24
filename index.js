17e8c0dffc39d72d1e2fe61aa7ce48540ff2cbe2
Ninad Ambade <ninad.ambade@aploris.com>
2022-04-24 17:50:50 +0530

    Sign up page implementation

--- index.js
+++ index.js
@@ -2,11 +2,25 @@ const express = require('express'); //Import the express dependency
 const app = express();              //Instantiate an express app, the main work horse of this server
 const port = 4000;                  //Save the port number where your server will be listenings
 var routes = require("./routes/routes");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
 app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
     console.log(`Now listening on port ${port}`); 
 });
 
+app.use(bodyParser.urlencoded({ extended: false })); // bodyparser before routes
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(routes);
+
+mongoose.connect("mongodb://localhost:27017/chat-app", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,  
+  })
+  .then(()=>{
+    console.log("DB connected successfully!!");
+  })
+  .catch((err)=>{
+    console.log(err);
+  });
--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,7 @@
 var express = require('express');
 const router = express();
-path = require('path')
+path = require('path');
+var User = require('../models/users');
 
 var index = path.join(__dirname, '../views/index.ejs')
 var signup = path.join(__dirname, '../views/signup.ejs')
@@ -11,5 +12,18 @@ router.get('/', (req, res) => {
 router.get('/signup', (req, res) => {
     res.render(signup);
 });
+router.post('/signup',(req, res, next)=>{
+        var username = req.body.name;
+        var password = req.body.password;
+        var email  = req.body.email;
 
+        var newUser = new User({
+                email: email,
+                username: username,
+                password: password
+			});
+        newUser.save(next);
+        return res.redirect('/');
+    }
+)
 module.exports = router;
