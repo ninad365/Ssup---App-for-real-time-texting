e5360aea6bb031b7320fba971cd28b2adc90395a
Ninad Ambade <ninad.ambade@aploris.com>
2022-04-30 05:33:19 +0530

    Create and Save session to the db

--- index.js
+++ index.js
@@ -4,15 +4,9 @@ const port = 4000;                  //Save the port number where your server wil
 var routes = require("./routes/routes");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
+var session = require('express-session');
+var MongoStore = require('connect-mongo');
 
-app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
-    console.log(`Now listening on port ${port}`); 
-});
-
-app.use(bodyParser.urlencoded({ extended: false })); // bodyparser before routes
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-app.use(routes);
 
 mongoose.connect("mongodb://localhost:27017/chat-app", {
     useNewUrlParser: true,
@@ -24,3 +18,23 @@ mongoose.connect("mongodb://localhost:27017/chat-app", {
   .catch((err)=>{
     console.log(err);
   });
+
+  app.use(session({
+    secret: 'work hard',
+    resave: true,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: "mongodb://localhost:27017/chat-app"
+  })
+  }));
+
+  app.use(bodyParser.urlencoded({ extended: false })); // bodyparser before routes
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(routes);
+
+app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
+  console.log(`Now listening on port ${port}`); 
+});
+
--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,6 @@
 var express = require('express');
 const router = express();
+var passport = require('passport');
 path = require('path');
 var User = require('../models/users');
 
@@ -9,6 +10,27 @@ var signup = path.join(__dirname, '../views/signup.ejs')
 router.get('/', (req, res) => {
     res.render(index);
 });
+router.post('/', function (req, res, next) {
+	console.log(req.body);
+	User.findOne({email:req.body.email},function(err,data){
+        console.log(data);
+		if(data){
+			
+			if(data.password==req.body.password){
+				//console.log("Done Login");
+				req.session.username = data._id;
+				//console.log(req.session.userId);
+				res.send({"Success":"Success!"});
+				
+			}else{
+				res.send({"Success":"Wrong password!"});
+			}
+		}else{
+			res.send({"Success":"This Email Is not regestered!"});
+		}
+	});
+});
+
 router.get('/signup', (req, res) => {
     res.render(signup);
 });
